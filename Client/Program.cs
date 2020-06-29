206b04297d8f371c4975295cf167031b80b0d3a1
Christian Weyer <christian.weyer@thinktecture.com>
2020-06-29 12:49:11 +0200

    Added shared models/DTOs

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Grpc.Net.Client;
-using Grpc.Net.Client.Web;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ConfTool.Client
 {
@@ -15,22 +17,7 @@ namespace ConfTool.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            builder.Services.AddSingleton(services =>
-            {
-                // Get the service address from appsettings.json
-                var config = services.GetRequiredService<IConfiguration>();
-                var backendUrl = config["BackendUrl"];
-
-                // Create a channel with a GrpcWebHandler that is addressed to the backend server.
-                //
-                // GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
-                // then GrpcWeb is recommended because it produces smaller messages.
-                var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
-
-                return GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions { HttpHandler = httpHandler });
-            });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
