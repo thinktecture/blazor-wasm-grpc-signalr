e3ff92562c2a188c13d269fedbb117179ba87ee7
Christian Weyer <christian.weyer@thinktecture.com>
2020-06-29 15:14:36 +0200

    Adding API Controller with SignalR support

--- Server/Model/AutoMapperProfile.cs
+++ Server/Model/AutoMapperProfile.cs
@@ -9,8 +9,11 @@ namespace ConfTool.Server.Model
         {
             CreateMap<Model.Conference, ConferenceOverview>();
             CreateMap<ConferenceOverview, Model.Conference>();
-            //CreateMap<Model.Conference, ConfTool.Shared.DTO.ConferenceDetails>();
-            //CreateMap<ConfTool.Shared.DTO.ConferenceDetails, Model.Conference>();
+
+            CreateMap<Model.Conference, ConfTool.Shared.DTO.ConferenceDetails>();
+            CreateMap<Model.Conference, ConfTool.Shared.DTO.ConferenceOverview>();
+            CreateMap<ConfTool.Shared.DTO.ConferenceDetails, Model.Conference>();
+            CreateMap<ConfTool.Shared.DTO.ConferenceOverview, Model.Conference>();
         }
     }
 }
